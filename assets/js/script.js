33b0baf51cc0530d42b991496d87d51485e26e47
Brunella D'Anzi <75045014+bdanzi@users.noreply.github.com>
2021-12-30 10:51:34 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ localStorage.setItem("stateMode",body.className);
 }
 button.className=localStorage.getItem("stateMode");
 function changeImage() {
-var buttom = document.getElementById("dark_light");
+var button = document.getElementById("dark_light");
 var currentClass_button = button.className;
 button.className = currentClass_button == "button primary color2 circle icon solid fa-moon" ? "button primary color2 circle icon solid fa-sun" : "button primary color2 circle icon solid fa-moon";
 localStorage.setItem("stateMode",button.className);
