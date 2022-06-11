fbe8ae3749c2487f9d8a888254d83d2791791da5
Brunella D'Anzi <75045014+bdanzi@users.noreply.github.com>
2022-06-11 16:10:27 +0200

    trial

--- codigotchi/script.js
+++ codigotchi/script.js
@@ -4,7 +4,7 @@ const micBtn = document.getElementById('microphone-codigotchi');
 const screen = document.getElementById('screen-codigotchi');
 const panelsData = document.getElementById('panels-data-codigotchi');
 
-const commands = ['mangia', 'balla', 'dormi','eat','dance','sleep'];
+const commands = ['mangia', 'balla', 'dormi'];
 const commands_it = ['mangia', 'balla', 'dormi'];
 const commands_en = ['eat','dance','sleep'];
 
@@ -23,17 +23,9 @@ function onResult(e) {
         console.log(testo)
         return testo.toLowerCase().includes(command);
     });
-    const action_translated = commands_en.find(function(command) {
-        if(action.includes(command)){
-            return commands_it[command];
-        }
-        else{
-            return action;
-        }
-    });
 
-    const actionClassname = 'codigotchi-screen_' + action_translated;
-    const audioElement = new Audio('codigotchi/sounds/' + action_translated + '.mp3');
+    const actionClassname = 'codigotchi-screen_' + action;
+    const audioElement = new Audio('codigotchi/sounds/' + action + '.mp3');
     audioElement.currentTime = 0;
     audioElement.play();
     screen.classList.add(actionClassname);
